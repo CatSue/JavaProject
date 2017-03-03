c4042eff055ce9601fabdec2bc8696b4d8da05f2
T'Chii <T'Chii>
2017-03-03 09:16:17 +0100

    Premier commit

---
+++ src/main/fr/epsi/poe/hardis/tu/Restaurant.java
@@ -0,0 +1,79 @@
+package fr.epsi.poe.hardis.tu;
+
+/**
+ * Restaurant : classe utilisée pour faire des TU
+ * Restaurant = nom, codePostal, telephone
+ *
+ * @version : 1.0
+ * @author : Marie PÉTROD
+ */
+
+public class Restaurant {
+    //Nom du restaurant
+    private String nom;
+
+    //Adresse du restaurant
+    private String adresse;
+
+    //Code postal du restaurant
+    private String codePostal;
+
+    //N° de téléphone du restaurant
+    private String telephone;
+
+    //Ouverture ou fermeture du restaurant
+    private boolean ouverture = true;
+
+    //*****CONSTRUCTEURS*****//
+    public Restaurant(boolean ouverture) {
+        ouverture = true;
+    }
+
+    public Restaurant() {
+    }
+
+    //*****GETTEURS*****//
+    public String getNom() {
+        return nom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public String getCodePostal() {
+        return codePostal;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public boolean isOuverture() {
+        return ouverture;
+    }
+
+    //*****SETTEURS*****//
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public void setCodePostal(String codePostal) {
+        this.codePostal = codePostal;
+    }
+
+    public void setTelephone(String pTelephone) {
+        //vérifier si le téléphone n'est pas nul, n'est pas vide et commence bien par +33
+        if (pTelephone != null && !"".equals(pTelephone) && pTelephone.startsWith("+33")){
+            telephone = pTelephone;
+        }
+    }
+
+    public void setOuverture(boolean ouverture) {
+        this.ouverture = ouverture;
+    }
+}
---
+++ src/test/fr/epsi/poe/hardis/tu/RestaurantTest.java
@@ -0,0 +1,63 @@
+package fr.epsi.poe.hardis.tu;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by T'Chi on 28/02/2017.
+ */
+
+class RestaurantTest {
+
+    //vérification qu'un restaurant est ouvert par defaut
+    @Test
+    public void testThatRestaurantIsOpenByDefault() {
+        /**
+         * Arrange
+         * Créer le contexte du test, création des objets nécessaires à l'exécution du test
+         * Utilisation du constructeur par defaut
+         */
+        Restaurant restaurant = new Restaurant();
+
+        /**
+         * Act
+         * action sur l'objet, on va vérifier que le restaurant est ouvert
+         */
+        boolean etat = restaurant.isOuverture();
+
+        /**
+         * Assert
+         */
+        Assertions.assertTrue(etat); //vérifie que l'état est bien vrai
+    }
+
+    //vérification quand on set n° de téléphone si le n° commence par +33 sauvegarde
+    @Test
+    public void testThatSaveTheValidNumber() {
+        // Arrange
+        Restaurant restaurant = new Restaurant();
+        // Act
+        restaurant.setTelephone("+33605020305");
+        // Assert
+        Assertions.assertEquals("+33605020305", restaurant.getTelephone());
+    }
+
+    //vérification quand on set n° de téléphone si le n° ne commence par +33 pas de sauvegarde
+    @Test
+    public void testThatNotSaveTheInvalidNumber() {
+        // Arrange
+        Restaurant restaurant = new Restaurant();
+        // Act
+        restaurant.setTelephone("0605020305");
+        // Assert
+        Assertions.assertNotEquals("0605020305", restaurant.getTelephone());
+    }
+
+    //vérification qu'il est possible de stocker des départements corses (cas limite)
+    @Test
+    public void testThatCorseIsOk() {
+
+    }
+}
